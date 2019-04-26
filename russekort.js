e9f78082de01b5984fb305c7eea9aec172e51064
Jonas Emanuel Gilje <jonas@jonasgilje.no>
2019-04-26 17:46:32 +0200

    Finished from screenshot

---
+++ russekort.js
@@ -0,0 +1,51 @@
+function Russekort(data) {
+  this.show = () => {
+    createCanvas(data.size.x, data.size.y);
+    background(...data.color);
+    // middle
+    push();
+    strokeWeight(data.size.s);
+    for (i = data.size.h; i > 0; i--) {
+      stroke(...this.colorFunction(i, data.size.h, data.size.p, ...data.color));
+      ellipse(data.size.x / 2, data.size.y / 2, i);
+    }
+
+    pop();
+    // image
+    push();
+    fill(0);
+    translate(...data.image.pos);
+    const angleDegrees = (-data.image.angle / 180.0) * PI;
+    rotate(angleDegrees);
+    strokeWeight(data.image.s);
+    rect(0, 0, ...data.image.size);
+    translate(...data.image.translate.map(x => -x));
+    fill(...data.image.maskColor);
+    rect(0, 0, ...data.image.size);
+    pop();
+    this.populateImage(data.image);
+    // text
+    data.text.forEach(element => {
+      this.renderText(element);
+    });
+  };
+
+  this.populateImage = image => {
+    for (let i = image.xRange[0]; i < image.xRange[1]; i++) {
+      for (let j = image.yRange[0]; j < image.yRange[1]; j++) {
+        set();
+      }
+    }
+  };
+
+  this.colorFunction = (...args) => {
+    const factor = Math.pow(args[0] / args[1], args[2]);
+    return [
+      255 + (args[3] - 255) * factor,
+      255 + (args[4] - 255) * factor,
+      255 + (args[5] - 255) * factor
+    ];
+  };
+
+  this.renderText = () => {};
+}
--- sketch.js
+++ sketch.js
@@ -1,22 +1,30 @@
-let russekort;
+let russekort,
+  imgArray = [];
+const imgSrcData = ["img/jonas.png", "img/logo.png"];
+
+function preload() {}
 
 function setup() {
   // put preload code here
+  frameRate(1);
   const data = {
     name: "Jonas' russekort",
-    size: { x: 1200, y: 700, h: 550 },
-    color: { r: 236, g: 27, b: 46 },
+    size: { x: 1200, y: 700, h: 550, s: 2, p: 0.8 },
+    color: [236, 27, 46],
     font: "Arial",
     image: {
-      src: "jonas.png",
+      srcIndex: 0,
       pos: [80, 180],
       size: [390, 510],
       translate: [45, 45],
       angle: 10.0,
-      power: 0.8
+      maskColor: [0, 255, 0],
+      s: 15,
+      xRange: [100, 500],
+      yRange: [150, 300]
     },
     logo: {
-      src: "logo.png",
+      srcIndex: 1,
       pos: [100, 600],
       size: [100, 500]
     },
@@ -34,13 +42,18 @@ function setup() {
         pos2: [1200, 100]
       },
       {
-        text: "Tlf: 116 111\nAdresse: jonas@jonasgilje.no\nMedlem av: Foreningen for stølsliv og friluftsliv i indre Gjesdal\n\n'k Heb slaaptekort want ik slaap te kort\nEsto es una fiesta de locos, pero yo soy el único que no estoy loco",
+        text:
+          "Tlf: 116 111\nAdresse: jonas@jonasgilje.no\nMedlem av: Foreningen for stølsliv og friluftsliv i indre Gjesdal\n\n'k Heb slaaptekort want ik slaap te kort\nEsto es una fiesta de locos, pero yo soy el único que no estoy loco",
         align: "CENTER",
         pos1: [0, 0],
         pos2: [1200, 100]
-      },
+      }
     ]
   };
+
+  russekort = new Russekort(data);
+
+  russekort.show();
 }
 
 function draw() {
